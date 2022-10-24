ae7fde4cef8978303a7d3bedcec9c42a3dfe8321
Dimanikis <dlimagao@gmail.com>
2022-10-24 12:55:15 +0300

    Revert "Cors off v0.2"
    This reverts commit 1d9b1dc193f5ea5948ae46d5bf22e8d16c70d874.

--- src/main/java/arc/ua/arcshop/configs/SecurityConfig.java
+++ src/main/java/arc/ua/arcshop/configs/SecurityConfig.java
@@ -36,7 +36,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
-                .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/")

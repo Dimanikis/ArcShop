46d933b4c318129df95059ff65f341a2e119de42
Dimanikis <dlimagao@gmail.com>
2022-10-24 15:56:02 +0300

    Cors of v1.2

--- src/main/resources/Static/js/goto.js
+++ src/main/resources/Static/js/goto.js
@@ -1,13 +1,3 @@
 function goToURL(url) {
     location.href = url;
-}async function goToURL(url) {
-    let response = await fetch("https://arc-shop.herokuapp.com/perform_login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({"name" : "user123", "password" : "123"}),
-    });
-    let result = await response.json();
-    alert(result);
 }
